804a06f77485cec11eb8df2b06b39889be49eed8
Gabriel Neiva <gabrielksneivagi@gmail.com>
2025-12-04 21:42:36 -0300

    test: trigger E2E with full IAM permissions (SQS, CloudWatch)

--- cmd/lambda/main.go
+++ cmd/lambda/main.go
@@ -1,7 +1,7 @@
 package main
 
 // Lambda handler for EVM transaction execution
-// Updated: 2025-12-04 23:05 - Testing E2E fixes
+// Updated: 2025-12-04 23:10 - Testing E2E with full IAM permissions
 
 import (
 	"context"
